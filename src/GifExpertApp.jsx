c47f44b30a9f78163488f37b8c0277941d162626
Rama <ramiroquesadaperez@gmail.com>
2023-03-25 06:08:25 -0300

    Proyecto finalizado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { AddCategory } from "./Components/AddCategory";
-import { GifGrid } from "./Components/GifGrid";
+import { AddCategory, GifGrid } from "./Components";
+
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["GIFS"]);
