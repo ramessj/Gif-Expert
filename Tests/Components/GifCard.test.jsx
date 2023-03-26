5c73d308b53c5ce4d37731e45753f76ca6dae9c9
Rama <ramiroquesadaperez@gmail.com>
2023-03-26 03:42:05 -0300

    Tests en GifCard completados

--- Tests/Components/GifCard.test.jsx
+++ Tests/Components/GifCard.test.jsx
@@ -1,11 +1,11 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import  {GifCard}  from "../../src/Components"
 
 describe('Pruebas en <GifCard />', () => {
 
-  const title = ''
+  const title = 'prueba'
 
-  const url = 'https://url.com'
+  const url = 'https://media1.giphy.com/dadada.gif'
 
   test('Debe hacer match con el snapshot', () => {
     const {container} = render(<GifCard title={title} url={url}/>)
@@ -13,4 +13,26 @@ describe('Pruebas en <GifCard />', () => {
     expect(container).toMatchSnapshot();
 
   })
+
+  test('Debe mostrar la imagen con la URL y el ALT indicado', () => {
+
+    render(<GifCard title={title} url={url} />)
+
+    const { src, alt } = screen.getByRole('img');
+
+    expect( src ).toBe( url )
+    expect( alt ).toBe( title )
+
+  })
+
+  test('Debe de mostrar el titulo en el componente', () => {
+
+    render(<GifCard title={title} url={url} />)
+
+    expect( screen.getByText(title)).toBeTruthy()
+  })
+
+
+
+
 })
